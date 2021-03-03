f3bdadb8c1d30d30280f1db608b24bf1b6b7bd87
Tianwes <willnotsuffice@gmail.com>
2021-03-03 08:32:41 -0500

    Got notes's css to display

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -10,17 +10,30 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// GET method route
+// in order to have css display
+app.use(express.static('../css'))
 
+// GET method route
 // works 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html/')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
 
-// POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-})
+// POST method - USER SENDS DATA TO US THAT THEY WANT TO SAVE
+// (THEIR NOTES)
+// app.post('/', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
+
+// MAYBE USE THIS FOR DELETING 
+// app.post('/api/clear', (req, res) => {
+//     // Empty out the arrays of data
+//     tableData.length = 0;
+//     waitListData.length = 0;
+
+//     res.json({ ok: true });
+//   });
+// };
 
 // Create our server
 app.listen(PORT, () => console.log(`App listening on: http://localhost:${PORT}`))
