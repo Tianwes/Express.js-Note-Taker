552515fa073bd7cd97c006c717aff5e7599e824c
Tianwes <willnotsuffice@gmail.com>
2021-03-06 15:30:55 -0500

    Changed /notes file path, Heroku not finding path

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html/')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html/')));
+app.get('/notes', (req, res) => res.sendFile(path.sendFile('./public/notes.html/')));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
